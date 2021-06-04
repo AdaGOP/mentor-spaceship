e20f4ed033594cfa51715e64e4dae8da0defaa27
Haryanto Salim <quang.haryanto@gmail.com>
2021-06-04 19:03:47 +0700

    (rebased)prepare an IBAction func to navigate from control/main to electrical and vice versa

---
+++ mentor-spaceshipacademy/ElectricalViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ElectricalViewController.swift
+//  mentor-spaceshipacademy
+//
+//  Created by Haryanto Salim on 03/06/21.
+//
+
+import UIKit
+
+class ElectricalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func navigateToControl(_ sender: UIButton) {
+        //code to navigate to Control Room
+        
+    }
+    
+    @IBAction func navigateToFuel(_ sender: UIButton) {
+        //TODO: talk to the owner of Fuel Page
+    }
+    
+}
